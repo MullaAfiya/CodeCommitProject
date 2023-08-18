882f2611a7840fea53a9f2b0a40395f0fee1b94e
MullaAfiya <135946533+MullaAfiya@users.noreply.github.com>
2023-08-18 15:47:37 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws s3 cp s3://awsbucketforcodedeploy/CodeBuild/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /home/ec2-user/apache-tomcat-8.5.92/webapps
-sudo /home/ec2-user/apache-tomcat-8.5.92/bin/startup.sh
-chmod -R 777 /home/ec2-user/apache-tomcat-8.5.92/webapps/LoginWebApp-1.war
