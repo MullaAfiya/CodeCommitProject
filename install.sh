c2f163aed98eac969fe96e63993f463ec2806d24
MullaAfiya <135946533+MullaAfiya@users.noreply.github.com>
2023-08-18 13:02:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-aws s3 cp s3://kc-buck-123/kc-build/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /home/ec2-user/webserver/apache-tomcat-9.0.79/webapps
-sudo /home/ec2-user/webserver/apache-tomcat-9.0.79/bin/startup.sh
-chmod -R 777 /home/ec2-user/webserver/apache-tomcat-9.0.79/webapps/LoginWebApp-1.war
+aws s3 cp s3://awsbucketforcodedeploy/CodeBuild/LoginWebApp-1.war /tmp
+sudo mv /tmp/LoginWebApp-1.war /home/ec2-user/webserver/apache-tomcat-8.5.92/webapps
+sudo /home/ec2-user/webserver/apache-tomcat-8.5.92/bin/startup.sh
+chmod -R 777 /home/ec2-user/webserver/apache-tomcat-8.5.92/webapps/LoginWebApp-1.war
